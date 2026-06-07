c6d9f96d3b8995765a83e0b64b89f3cf7a7eb885
Zahwan Ardy <zahwanardy@gmail.com>
2026-06-08 02:43:47 +0700

    google

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://www.hifi-indosat.site', // Ganti dengan domain Anda jika berbeda
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+  ]
+}
